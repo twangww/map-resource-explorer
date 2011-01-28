9e1cb6038069e9dc30e507a09a1e7440c1e249a6
duchangyu <duchangyu@gmail.com>
2011-01-28 03:13:22 +0000

    Add EventManager class, register event when initlizing.

---
+++ MapResourceExplorer/Model/EventManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using MapResourceExplorer.UI;
+
+namespace MapResourceExplorer.Model
+{
+    class EventManager
+    {
+
+        #region Singleton
+        private static EventManager _instance;
+
+        private EventManager()
+        {
+
+        }
+
+        public static EventManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new EventManager();
+                }
+
+                return _instance;
+            }
+            
+        }
+
+        #endregion 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void RegisterEvents()
+        {
+            Application.DocumentManager.DocumentActivated += new DocumentCollectionEventHandler(DocumentManager_DocumentActivated);
+            
+        }
+        /// <summary>
+        /// Refresh resource tree when active document is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void DocumentManager_DocumentActivated(object sender, DocumentCollectionEventArgs e)
+        {
+            ResourceExplorerPalette.Instance.ExplorerForm.ForceRefresh();
+        }
+    }
+
+}
