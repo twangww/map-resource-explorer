25d72d72596770ac780b6cb7b3f9188809bc5c20
duchangyu@gmail.com <duchangyu@gmail.com@d07f4925-1d4c-a52a-f837-d8bfc931ae01>
2011-02-08 11:27:48 +0000

    Add xml validate and resource update. xml validatation not completed.

--- trunk/MapResourceExplorer/UI/XmlEditor.cs
+++ trunk/MapResourceExplorer/UI/XmlEditor.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using MapResourceExplorer.Model;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace MapResourceExplorer.UI
 {
@@ -33,6 +36,19 @@ namespace MapResourceExplorer.UI
                 return _instance;
             }
 
+        }
+
+        private string _currentResourceId;
+        public string CurrentResourceId
+        {
+            get
+            {
+                return _currentResourceId;
+            }
+            set
+            {
+                _currentResourceId = value;
+            }
         }
 
         public void SetXml(string xml)
@@ -88,14 +104,118 @@ namespace MapResourceExplorer.UI
             tbXmlEditor.Text = string.Empty;
         }
 
+
+
         private void toolStripButtonValidate_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Validate not implemented");
+            //MessageBox.Show("Validate not implemented");
+            //string errMsg = string.Empty;
+            //bool isValid = ResourceManager.Instance.ValidateResource(this.CurrentResourceId, tbXmlEditor.Text,ref errMsg);
+            //if (isValid == false)
+            //{
+            //    MessageBox.Show("XML is not valid.\n"+errMsg);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("XML is valid.");
+            //}
+
+            if (!xmlValidated)
+            {
+                string xsdPath = ResourceManager.Instance.GetSchemaFilePath(this.CurrentResourceId);
+                this.XmlValidate(xsdPath, tbXmlEditor.Text);
+            }
         }
 
         private void toolStripButtonSaveToLibrary_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Save to library not implemented");
+            if (!xmlValidated)
+            {
+                //TODO: Get xsd path
+                string xsdPath = ResourceManager.Instance.GetSchemaFilePath(this.CurrentResourceId);
+                this.XmlValidate(xsdPath, tbXmlEditor.Text);
+            }
+
+            if (xmlValid)
+            {
+                ResourceManager.Instance.SetResourceContent(this.CurrentResourceId, tbXmlEditor.Text);
+                MessageBox.Show("Resource Content is updated into Library");
+            }
+
         }
+
+        private void tbXmlEditor_TextChanged(object sender, EventArgs e)
+        {
+            xmlValidated = false;
+        }
+
+        #region XML validation
+        //whether the xml is checked or not
+        private bool xmlValidated = false;
+        //whether the xml is valid or not, against xsd
+        private bool xmlValid = false;
+
+        private XmlTextReader Reader;
+        private void XmlValidate(string xsdPath, string xmlContent)
+        {
+            try
+            {
+                // 1- Read XML file content
+                this.Reader = new XmlTextReader(new StringReader(xmlContent)); ;
+
+                // 2- Read Schema file content
+                StreamReader SR = new StreamReader(xsdPath);
+
+                // 3- Create a new instance of XmlSchema object
+                XmlSchema Schema = new XmlSchema();
+                // 4- Set Schema object by calling XmlSchema.Read() method
+                Schema = XmlSchema.Read(SR,
+                    new ValidationEventHandler(ReaderSettings_ValidationEventHandler));
+
+                // 5- Create a new instance of XmlReaderSettings object
+                XmlReaderSettings ReaderSettings = new XmlReaderSettings();
+                // 6- Set ValidationType for XmlReaderSettings object
+                ReaderSettings.ValidationType = ValidationType.Schema;
+                // 7- Add Schema to XmlReaderSettings Schemas collection
+                ReaderSettings.Schemas.Add(Schema);
+
+                // 8- Add your ValidationEventHandler address to
+                // XmlReaderSettings ValidationEventHandler
+                ReaderSettings.ValidationEventHandler +=
+                    new ValidationEventHandler(ReaderSettings_ValidationEventHandler);
+
+                // 9- Create a new instance of XmlReader object
+                XmlReader objXmlReader = XmlReader.Create(Reader, ReaderSettings);
+
+                // 10- Read XML content in a loop
+                while (objXmlReader.Read())
+                { /*Empty loop*/}
+
+                xmlValid = true;
+                xmlValidated = true;
+
+            }//try
+            // Handle exceptions if you want
+            catch (UnauthorizedAccessException AccessEx)
+            {
+                throw AccessEx;
+            }//catch
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }//catch
+        }
+
+        private void ReaderSettings_ValidationEventHandler(object sender,
+    ValidationEventArgs args)
+        {
+            // 11- Implement your logic for each validation iteration
+            string strTemp;
+            strTemp = "Line: " + this.Reader.LineNumber + " - Position: "
+                + this.Reader.LinePosition + " - " + args.Message;
+
+            this.tbValidateResult.Text += strTemp;
+        }
+        #endregion
     }
 }
