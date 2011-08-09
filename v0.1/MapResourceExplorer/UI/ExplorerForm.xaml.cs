f96deaddd290780fb742fa5acb5de83b2bfe7330
duchangyu@gmail.com <duchangyu@gmail.com@d07f4925-1d4c-a52a-f837-d8bfc931ae01>
2011-08-09 06:59:20 +0000

    v0.1

---
+++ v0.1/MapResourceExplorer/UI/ExplorerForm.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MapResourceExplorer.Model;
+using System.Collections;
+
+namespace MapResourceExplorer.UI
+{
+    /// <summary>
+    /// Interaction logic for ExplorerForm.xaml
+    /// </summary>
+    public partial class ExplorerForm : UserControl
+    {
+        const string TAG_IS_RESOURCE = "IsResource";
+
+        public ExplorerForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ForceRefresh()
+        {
+            if (this.IsVisible)
+            {
+                BindTreeView(treeView1);
+            }
+
+        }
+
+
+        private void BindTreeView(TreeView tree)
+        {
+            tree.Items.Clear();
+
+            ResourceManager resourceMgr = ResourceManager.Instance;
+
+            Dictionary<string, string> resourceTypes = resourceMgr.GetResourceAllTypes();
+            foreach (var resType in resourceTypes)
+            {
+                TreeViewItem resourceTypeitem = new TreeViewItem();
+                resourceTypeitem.Header = resType.Key;
+                resourceTypeitem.ToolTip = resType.Value;
+                resourceTypeitem.Tag = string.Empty;
+                resourceTypeitem.IsExpanded = true;
+                //Bind resource to resourceItemType
+                Dictionary<string, string> resList = resourceMgr.GetResourcesByType(resType.Key);
+
+                foreach (var item in resList)
+                {
+                    TreeViewItem resItem = new TreeViewItem();
+                    resItem.Header = item.Key;
+                    resItem.ToolTip = item.Value;
+                    resItem.Tag = TAG_IS_RESOURCE;
+
+                    resourceTypeitem.Items.Add(resItem);
+                }
+
+                tree.Items.Add(resourceTypeitem);
+            }
+        }
+
+
+        #region treeview righ click to select item
+        /// <summary>
+        /// right click Item to select item. for context menu
+        /// 
+        /// http://www.cnblogs.com/TianFang/archive/2010/02/10/1667153.html
+        /// http://www.cnblogs.com/tianfang/archive/2010/02/10/1667186.html
+        /// </summary>
+        /// 
+
+
+        //bool isResItemSlected = false;
+
+        private void treeView1_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //isResItemSlected = false;
+        }
+
+        private void TreeViewItem_MouseRightButtonDown(Object sender, MouseButtonEventArgs e)
+        {
+            //TreeViewItem treeViewItem = sender as TreeViewItem;
+            //if (treeViewItem.Tag.ToString() == TAG_IS_RESOURCE)
+            //{
+            //    isResItemSlected = true;
+            //}
+        }
+        private void TreeViewItem_PreviewMouseRightButtonDown(Object sender, MouseButtonEventArgs e)
+        {
+            var treeViewItem = VisualUpwardsSearch<TreeViewItem>(e.OriginalSource as DependencyObject) as TreeViewItem;
+            if (treeViewItem != null)
+            {
+                treeViewItem.Focus();
+
+                e.Handled = true;
+            }
+        }
+
+        static DependencyObject VisualUpwardsSearch<T>(DependencyObject source)
+        {
+            while (source != null && source.GetType() != typeof(T))
+            {
+                source = VisualTreeHelper.GetParent(source);
+            }
+
+            return source;
+        }
+
+        #endregion
+
+        private void RefreshButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            ForceRefresh();
+        }
+
+        private void ShowResourceContent_Clicked(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = treeView1.SelectedItem as TreeViewItem;
+            if (item != null && item.Tag.ToString() == TAG_IS_RESOURCE)
+            {
+                string resId = item.ToolTip.ToString();
+                string resXml = ResourceManager.Instance.GetResourceContent(resId);
+                XmlEditor.Instance.CurrentResourceId = resId;
+                XmlEditor.Instance.SetXml(resXml);
+                XmlEditor.Instance.ShowDialog();
+            }
+        }
+
+        private void ShowResourceReferences_Clicked(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = treeView1.SelectedItem as TreeViewItem;
+            if (item != null && item.Tag.ToString() == TAG_IS_RESOURCE)
+            {
+                string resId = item.ToolTip.ToString();
+                string resXml = ResourceManager.Instance.GetResourceReferences(resId);
+                XmlEditor.Instance.CurrentResourceId = resId;
+                XmlEditor.Instance.SetXml(resXml);
+                XmlEditor.Instance.ShowDialog();
+            }
+
+        }
+
+
+    }
+}
---
+++ v0.1/MapResourceExplorer/UI/XmlEditor.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+using MapResourceExplorer.Model;
+
+namespace MapResourceExplorer.UI
+{
+    public partial class XmlEditor : Form
+    {
+        private XmlEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static XmlEditor _instance;
+
+        public static XmlEditor Instance
+        {
+            get
+            {
+
+                if (_instance == null)
+                {
+                    _instance = new XmlEditor();
+                    _instance.tbXmlEditor.Text = string.Empty;
+                }
+
+                return _instance;
+            }
+
+        }
+
+        private string _currentResourceId;
+        public string CurrentResourceId
+        {
+            get
+            {
+                return _currentResourceId;
+            }
+            set
+            {
+                _currentResourceId = value;
+            }
+        }
+
+        public void SetXml(string xml)
+        {
+            _instance.tbXmlEditor.Text = xml;
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            //Save xml to file
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog1.FileName;
+                using (StreamWriter sw = new StreamWriter(fileName, true))
+                {
+                    sw.WriteLine(tbXmlEditor.Text);
+                }
+            }
+
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                string fileContents;
+                using (StreamReader sr = new StreamReader(@fileName))
+                {
+                    fileContents = sr.ReadToEnd();
+                }
+
+                tbXmlEditor.Text = fileContents;
+            }
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbXmlEditor.Text);
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                tbXmlEditor.Text = Clipboard.GetText();
+            }
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbXmlEditor.Text);
+            tbXmlEditor.Text = string.Empty;
+        }
+
+
+
+        private void toolStripButtonValidate_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Validate not implemented");
+            //string errMsg = string.Empty;
+            //bool isValid = ResourceManager.Instance.ValidateResource(this.CurrentResourceId, tbXmlEditor.Text,ref errMsg);
+            //if (isValid == false)
+            //{
+            //    MessageBox.Show("XML is not valid.\n"+errMsg);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("XML is valid.");
+            //}
+
+            if (!xmlValid)
+            {
+                string xsdPath = GetSchemaFilePath();
+                this.XmlValidate(xsdPath, tbXmlEditor.Text);
+            }
+        }
+
+        private void toolStripButtonSaveToLibrary_Click(object sender, EventArgs e)
+        {
+
+            if (!xmlValid)
+            {
+                DialogResult ds = MessageBox.Show("The resource xml has not been validated! Are you going to validate now?", "Caution", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (ds == DialogResult.Yes)
+                {
+                    //TODO: Get xsd path
+                    string xsdPath = GetSchemaFilePath();
+                    this.XmlValidate(xsdPath, tbXmlEditor.Text);
+                }
+                else if (ds == DialogResult.No)
+                {
+                    if (DialogResult.Yes == MessageBox.Show("Do you really want to update to repository without validating? It may cause problems.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
+                    {
+                        UpdateToRepository();
+                    }
+                }
+            }
+            else
+            {
+                UpdateToRepository();
+            }
+
+        }
+
+        private void UpdateToRepository()
+        {
+            ResourceManager.Instance.SetResourceContent(this.CurrentResourceId, tbXmlEditor.Text);
+            MessageBox.Show("Resource Content is updated into Library");
+        }
+
+        private void tbXmlEditor_TextChanged(object sender, EventArgs e)
+        {
+            xmlValid = false;
+        }
+
+        private string GetSchemaFilePath()
+        {
+            ////TODO: validate resource content according to resoruce type
+            //MgResourceIdentifier resId = new MgResourceIdentifier(resourceId);
+            //string resType = resId.GetResourceType();
+            ////.....
+
+            //XmlDocument dom = new XmlDocument();
+            //dom.LoadXml(tbXmlEditor.Text);
+            
+            
+            return Properties.Settings.Default.SchemaPath;
+        }
+
+        #region XML validation
+        //whether the xml is valid or not, against xsd
+        private bool xmlValid = false;
+
+        private XmlTextReader Reader;
+        private void XmlValidate(string xsdPath, string xmlContent)
+        {
+            try
+            {
+                // 1- Read XML file content
+                this.Reader = new XmlTextReader(new StringReader(xmlContent)); ;
+
+                // 2- Read Schema file content
+                StreamReader SR = new StreamReader(xsdPath);
+
+                // 3- Create a new instance of XmlSchema object
+                XmlSchema Schema = new XmlSchema();
+                // 4- Set Schema object by calling XmlSchema.Read() method
+                Schema = XmlSchema.Read(SR,
+                    new ValidationEventHandler(ReaderSettings_ValidationEventHandler));
+
+                // 5- Create a new instance of XmlReaderSettings object
+                XmlReaderSettings ReaderSettings = new XmlReaderSettings();
+                // 6- Set ValidationType for XmlReaderSettings object
+                ReaderSettings.ValidationType = ValidationType.Schema;
+                // 7- Add Schema to XmlReaderSettings Schemas collection
+                ReaderSettings.Schemas.Add(Schema);
+
+                // 8- Add your ValidationEventHandler address to
+                // XmlReaderSettings ValidationEventHandler
+                ReaderSettings.ValidationEventHandler +=
+                    new ValidationEventHandler(ReaderSettings_ValidationEventHandler);
+
+                // 9- Create a new instance of XmlReader object
+                XmlReader objXmlReader = XmlReader.Create(Reader, ReaderSettings);
+
+                // 10- Read XML content in a loop
+                while (objXmlReader.Read())
+                { /*Empty loop*/}
+
+                xmlValid = true;
+
+            }//try
+            // Handle exceptions if you want
+            catch (UnauthorizedAccessException AccessEx)
+            {
+                throw AccessEx;
+            }//catch
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }//catch
+        }
+
+        private void ReaderSettings_ValidationEventHandler(object sender,
+    ValidationEventArgs args)
+        {
+            // 11- Implement your logic for each validation iteration
+            string strTemp;
+            strTemp = "Line: " + this.Reader.LineNumber + " - Position: "
+                + this.Reader.LinePosition + " - " + args.Message;
+
+            this.tbValidateResult.Text += strTemp;
+
+            xmlValid = false;
+        }
+        #endregion
+    }
+}
